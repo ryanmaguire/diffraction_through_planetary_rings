0eca050909fe32f95a28f9765b91148fbe63c7e1
ryanmaguire <rmaguire@protonmail.com>
2026-01-03 15:22:47 -0500

    Cleaning up, using values in common.py.

--- pysrc/fraunhofer_diffraction_airy_disks.py
+++ pysrc/fraunhofer_diffraction_airy_disks.py
@@ -2,20 +2,20 @@
 ################################################################################
 #                                   LICENSE                                    #
 ################################################################################
-#   This file is part of newtonian_black_holes.                                #
+#   This file is part of diffraction_through_planetary_rings.                  #
 #                                                                              #
-#   newtonian_black_holes is free software: you can redistribute it and/or     #
-#   modify it under the terms of the GNU General Public License as published   #
-#   by the Free Software Foundation, either version 3 of the License, or       #
-#   (at your option) any later version.                                        #
+#   diffraction_through_planetary_rings is free software: you can redistribute #
+#   it and/or modify it under the terms of the GNU General Public License as   #
+#   published by the Free Software Foundation, either version 3 of the         #
+#   License, or (at your option) any later version.                            #
 #                                                                              #
-#   newtonian_black_holes is distributed in the hope that it will be useful,   #
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   diffraction_through_planetary_rings is distributed in the hope that it     #
+#   will be useful, but WITHOUT ANY WARRANTY; without even the implied         #
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  #
 #   GNU General Public License for more details.                               #
 #                                                                              #
 #   You should have received a copy of the GNU General Public License          #
-#   along with newtonian_black_holes.  If not, see                             #
+#   along with diffraction_through_planetary_rings.  If not, see               #
 #   <https://www.gnu.org/licenses/>.                                           #
 ################################################################################
 #   Purpose:                                                                   #
@@ -28,22 +28,15 @@
 # pylint wants all of the variables to be SCREAMING_CASE. Ignore this.
 # pylint: disable = invalid-name
 
+# pylint also has difficulty determining what's inside a C module. Ignore.
+# pylint: disable = c-extension-no-member
+
 # Note: ALL UNITS ARE IN METERS.
-import numpy
-import matplotlib.pyplot as plt
+import math
 import tmpyl
+import common
 
-# Set I0 to 1 for simplicity.
-initial_intensity = 1.0
-
-# Clip to make dark colors a little brighter.
-clip = 0.025 * initial_intensity
-
-# We again use red light.
-wavelength = 6.5E-07
-
-# Parameters for the aperture.
-# It is circular, we only need the radius.
+# Parameters for the aperture. It is circular, we only need the radius.
 radius = 5.0E-04
 
 # Distance between the aperture and the screen.
@@ -51,48 +44,36 @@ radius = 5.0E-04
 distance = 5.0E-01
 
 # Scale factor for the independent variable.
-factor = 2.0 * numpy.pi * radius / (distance * wavelength)
+factor = 2.0 * math.pi * radius / (distance * common.wavelength)
 
-# Parameters for the image.
-x_size = 5.0E-03
-y_size = 5.0E-03
-x_pixels = 2048
-y_pixels = 2048
-plot_boundaries = [-x_size, x_size, -y_size, y_size]
+# The title for the plot.
+title = "Fraunhofer Diffraction: Airy Disks"
 
-# Step sizes used for sampling the x and y axes.
-x_displacement = 2.0 * x_size / float(x_pixels)
-y_displacement = 2.0 * y_size / float(y_pixels)
+# Empty list for the intensity. We'll fill this as we go.
+intensity = []
 
-# Arrays for the two axes.
-x_vals = numpy.arange(-x_size, x_size, x_displacement)
-y_vals = numpy.arange(-y_size, y_size, y_displacement)
+# Loop through the points in the grid and compute the diffraction pattern.
+for x_val in common.x_vals:
 
-# Parameters for the image.
-x_grid, y_grid = numpy.meshgrid(x_vals, y_vals)
-plane_vals = numpy.sqrt(x_grid**2 + y_grid**2)
-arg = factor * plane_vals
+    # The output is a grid, which is represented by a list of lists. Create an
+    # empty list, we'll fill it with the current column in the next for-loop.
+    array = []
 
-intensity = initial_intensity * numpy.array(
-    [numpy.square(tmpyl.airy_j1(arr)) for arr in arg]
-)
+    # Loop through the elements of the current column.
+    for y_val in common.y_vals:
 
-# Make the plots.
-figure, axes = plt.subplots()
+        # The argument is given by the distance from the point to the origin,
+        # times the scale factor. Use Pythagoras to compute this.
+        argument = factor * tmpyl.sqrt(x_val ** 2 + y_val ** 2)
 
-image = axes.imshow(
-    intensity,
-    extent = plot_boundaries,
-    cmap = 'hot',
-    vmin = 0.0,
-    vmax = clip
-)
+        # We may use the Airy J1 function to compute the diffraction value.
+        diffraction_value = tmpyl.airy_j1(argument) ** 2
 
-# Add labels for the axes and the plot.
-axes.set_xlabel("x (meters)")
-axes.set_ylabel("y (meters)")
-axes.set_title("Fraunhofer Diffraction: Circular Aperture")
-figure.colorbar(image, ax = axes)
+        # Add the intensity to our array for the column.
+        array.append(common.initial_intensity * diffraction_value)
 
-# Render the plot.
-plt.savefig(__file__.rsplit('.', 1)[0] + ".png")
+    # Add the entire column to the intensity array.
+    intensity.append(array)
+
+# Create the plot.
+common.make_plots(intensity, title, __file__, clip = common.high_clip)
